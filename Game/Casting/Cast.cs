305fd4d3ac5c265050798e64bcd5ea85415b20a6
Mitchell Bower <mitchell@bowermail.net>
2022-02-23 18:28:42 -0700

    pretty much finished with this classes

---
+++ Game/Casting/Cast.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+
+namespace cse210_greed.Game.Casting{
+    public class Cast{
+        private Dictionary<string, List<Actor>> actors = new Dictionary<string, List<Actor>>();
+
+        /// <summary>
+        /// Constructor for Cast instances
+        /// </summary>
+        public Cast(){}
+
+        /// <summary>
+        /// Adds the given actor to the given group (makes a new group if group is unrecognized).
+        /// </summary>
+        /// <param name="group">The name of the group to be added to</param>
+        /// <param name="actor">The instance of Actor to add</param>            
+        public void AddActor(string group, Actor actor){ //if the key has not been added yet...
+            if (!actors.ContainsKey(group)){
+                actors[group] = new List<Actor>();
+            }
+
+            if (!actors[group].Contains(actor)){ //if the list associated with the key does not already contain the specified instance of actor...
+                actors[group].Add(actor);
+            }
+        }
+
+        /// <summary>
+        /// Returns all the actors in the cast.
+        /// </summary>
+        /// <param name="group">The group name</param>
+        /// <returns>the list of actors, or an empty list if there aren't any</returns>
+        public List<Actor> GetAllActors(string group){
+            List<Actor> results = new List<Actor>();
+            foreach (List<Actor> result in actors.Values){
+                results.AddRange(result);
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Takes the group name and returns the actors in the given group. Returns an empty list if there aren't any.
+        /// </summary>
+        /// <param name="group">The group name</param>
+        /// <returns>The actors in the given group, or an empty list if there aren't any</returns>
+        public List<Actor> GetActors(string group){
+            List<Actor> results = new List<Actor>();
+            if (actors.ContainsKey(group)){
+                results.AddRange(actors[group]);
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Returns the first actor in the given group
+        /// </summary>
+        /// <param name="group">The given group</param>
+        /// <returns>The first actor in the given group</returns>
+        public Actor GetFirstActor(string group){
+            Actor result = null;
+            if (actors.ContainsKey(group)){
+                if (actors[group].Count > 0){
+                    result = actors[group][0];
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Removes the given actor from the given group
+        /// </summary>
+        /// <param name="group">The given group</param>
+        /// <param name="actor">The given actor (will be removed)</param>
+        public void RemoveActor(string group, Actor actor){
+            if (actors.ContainsKey(group)){
+                actors[group].Remove(actor);
+            }
+        }
+    }
+}
---
+++ Game/Directing/Director.cs
@@ -0,0 +1,128 @@
+using System.Collections.Generic; //look into
+using cse210_greed.Game.Casting;
+using cse210_greed.Game.Services;
+
+namespace Unit04.Game.Directing{
+    /// <summary>
+    /// The person who directs the game.
+    /// Responsibility: Control the steps of the game.
+    /// </summary>
+    public class Director{
+        private KeyboardService keyboardService = null;
+        private VideoService videoService = null;
+        private int playerScore = 0;
+        Random random = new Random();
+        int COLS;
+        int ROWS;
+        int CELL_SIZE;
+
+
+        /// <summary>
+        /// Constructs an instance of director
+        /// </summary>
+        /// <param name="keyboardService">The passed instance of KeyboardService</param>
+        /// <param name="videoService">The passed instance of VideoService</param>
+        public Director(KeyboardService keyboardService, VideoService videoService)
+        {
+            this.keyboardService = keyboardService;
+            this.videoService = videoService;
+            CELL_SIZE = videoService.GetCellSize();
+            COLS = videoService.GetWidth() / CELL_SIZE;
+            ROWS = videoService.GetHeight() / CELL_SIZE;
+        }
+
+        /// <summary>
+        /// Starts the game loop for the passed cast
+        /// </summary>
+        /// <param name="cast">The passed instance 
+        /// of cast (should already be populated
+        /// with the proper actors)</param>
+        public void StartGame(Cast cast){
+            videoService.OpenWindow();
+            while (videoService.IsWindowOpen()){
+                GetInputs(cast);
+                DoUpdates(cast);
+                DoOutputs(cast);
+            }
+            videoService.CloseWindow();
+        }
+
+        /// <summary>
+        /// Gets directional input from the keyboard 
+        /// and applies the directional  to the robot
+        /// </summary>
+        /// <param name="cast">The passed instance of cast</param>
+        private void GetInputs(Cast cast){
+            Actor robot = cast.GetFirstActor("robot");
+            Location velocity = keyboardService.GetDirection();
+            robot.SetVelocity(velocity);
+        }
+
+        /// <summary>
+        /// Updates the robot and falling artifact positions 
+        /// and resolves collisions 
+        /// (i.e robot to artifact(s))
+        /// </summary>
+        /// <param name="cast">The passed instance of cast</param>
+        private void DoUpdates(Cast cast){
+            //declarations
+            List<Actor> banners = cast.GetActors("banner"); 
+                //index [0]: scoreBanner
+                Actor scoreBanner = banners[0];
+                //index [1]: dialogueBanner
+                Actor dialogueBanner = banners[1];
+            Actor robot = cast.GetFirstActor("robot");
+            List<Actor> artifacts = cast.GetActors("artifacts");
+            int maxX = videoService.GetWidth();
+            int maxY = videoService.GetHeight();
+
+            foreach (Actor actor in artifacts){
+                Artifact artifact = (Artifact) actor; //look into
+                bool isCaughtRecently = false;
+                
+                if (robot.GetPosition().Equals(artifact.GetPosition())){
+                    //set dialogue to the artifact's message.
+                    dialogueBanner.SetText(artifact.GetMessage());
+                    
+                    //score increase/decrease
+                    playerScore += artifact.GetPointValue();
+                    
+                    //move artifact to just above the top
+                    Location position = new Location(random.Next(1, COLS), 1);
+                    position = position.Scale(CELL_SIZE);
+                    artifact.SetPosition(position);
+                    isCaughtRecently = true;
+                }
+                
+                if(!isCaughtRecently){
+                    //the fall speed indicates the chance for delay in falling
+                    if(random.Next(0, artifact.GetFallSpeed() + 1) == 0){
+                        //move the artifact down
+                        artifact.SetVelocity(keyboardService.GetDirection("down"));
+                        artifact.MoveNext(maxX, maxY);
+                    }
+                        
+                    isCaughtRecently = false;
+                }
+            }
+            
+            //update player's score to the score banner
+            scoreBanner.SetText($"Score: {playerScore}");
+            //move robot
+            robot.MoveNext(maxX, maxY);
+        }
+
+        /// <summary>
+        /// Draws the actors on the display screen
+        /// </summary>
+        /// <param name="cast">The passed instance of cast</param>
+        private void DoOutputs(Cast cast){
+            //declare actors to be drawn
+            List<Actor> actors = cast.GetAllActors();
+            
+            videoService.ClearBuffer();
+            videoService.DrawActors(actors);
+            videoService.FlushBuffer();
+        }
+    }
+}
